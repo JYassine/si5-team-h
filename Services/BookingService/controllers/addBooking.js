a7a13b6ee3ddb2a710116b38148d64255ecf0f07
JYassine <yassine.jrad@etu.unice.fr>
2020-10-16 10:51:58 +0200

    Update addBooking.js

--- Services/BookingService/controllers/addBooking.js
+++ Services/BookingService/controllers/addBooking.js
@@ -3,11 +3,15 @@ const fileSync = require('lowdb/adapters/FileSync')
 const adapter = new fileSync('db.json')
 const db = low(adapter)
 
+db.defaults({ bookings: [] })
+    .write()
+
 const addBooking = (body) => {
 
     if (idAlreadyExist(body.id)) {
         throw Error("this id already exist in database");
     }
+    console.log(db.get("bookings").value())
     db.get("bookings").push(body).write();
 };
 
