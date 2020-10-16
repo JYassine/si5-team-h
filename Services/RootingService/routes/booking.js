4ecce0af091855e0740cbc983fcb9e78ecbd2088
Adrien <adrien.vasseur@etu.unice.fr>
2020-10-16 13:48:35 +0200

    #4 MàJ du rooting service Gère maintenant les routes GET /bookings et POST /bookings

--- Services/RootingService/routes/booking.js
+++ Services/RootingService/routes/booking.js
@@ -4,9 +4,26 @@ const axios = require('axios');
 const bookingRouter = express.Router();
 const registry = require("./registry");
 
+/**
+ * POST /bookings
+ * Payload: {
+ *     "id": string,
+ *     "idTravel": string
+ * }
+ */
 bookingRouter.route('/').post(async (req, res) => {
-    axios.post(registry.services.booking + 'book')
-        .then(response => res.status(200).send())
+    console.log(req.body);
+    axios.post(registry.services.booking.url + 'bookings', req.body)
+        .then(response => res.status(response.status).send())
+        .catch(err => console.log("Error sending request to booking service", err));
+});
+
+/**
+ * GET /bookings
+ */
+bookingRouter.route('/').get(async (req, res) => {
+    axios.get(registry.services.booking.url + "bookings")
+        .then(response => res.status(response.status).send(response.data))
         .catch(err => console.log("Error sending request to booking service", err));
 });
 
