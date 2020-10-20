e94164a1407a9996d299fbb5ad5ca5463e1a353c
JYassine <yassine.jrad@etu.unice.fr>
2020-10-20 18:04:49 +0200

    #7 Add test

--- Services/PaymentService/controllers/addOrderPayment.js
+++ Services/PaymentService/controllers/addOrderPayment.js
@@ -2,30 +2,32 @@ const low = require('lowdb')
 const fileSync = require('lowdb/adapters/FileSync')
 const adapter = new fileSync('db.json')
 const db = low(adapter)
-var uniqid = require('uniqid');
-const  pathLinkPayment = "http://localhost:4003/payment/execute/";
+const uniqid = require('uniqid');
+const pathLinkPayment = "http://localhost:4003/payment/execute/";
 
 db.defaults({ orderPayments: [] })
     .write()
 
-const addOrderPayment = (body) => {
+async function addOrderPayment(body) {
 
     let order = constructOrderPayment(body)
-    db.get("orderPayments").push(order).write();
-    return order
+    await db.get("orderPayments").push(order).write();
+
+    return order;
+
 };
 
 
 const constructOrderPayment = (body) => {
-    
+
     let idOrderPayment = uniqid();
     let order = {
-        id : idOrderPayment,
-        idBooking : body.idBooking,
-        currency : body.currency,
-        total : body.total,
-        payment_method : body.payment_method,
-        linkPayment : pathLinkPayment + body.payment_method + '-' + idOrderPayment
+        id: idOrderPayment,
+        idBooking: body.idBooking,
+        currency: body.currency,
+        total: body.total,
+        payment_method: body.payment_method,
+        linkPayment: pathLinkPayment + body.payment_method + '-' + idOrderPayment
     }
 
     return order;
