16f28d187ad05a68684c88e99976aafe5c532d3c
Couldosh <fabrice.kergaline.simon@gmail.com>
2020-10-27 16:00:22 +0100

    #19 Ajout gestions des options

--- Services/BookingService/controllers/addBooking.js
+++ Services/BookingService/controllers/addBooking.js
@@ -2,17 +2,29 @@ const low = require('lowdb')
 const fileSync = require('lowdb/adapters/FileSync')
 const adapter = new fileSync('db.json')
 const db = low(adapter)
+const axios = require('axios').default;
 
 db.defaults({ bookings: [] })
     .write()
 
-const addBooking = (body) => {
+const addBooking = async (body) => {
 
     if (idAlreadyExist(body.id)) {
         throw Error("this id already exist in database");
     }
+    const idTravel = body.idTravel
+    const options = body.options
+    body.price = await getPrice(idTravel, options)
     db.get("bookings").push(body).write();
+    console.log("bla")
 };
+
+async function getPrice(idTravel,options){
+    return await axios.post('http://localhost:4005',{
+        idTravel:idTravel,
+        options:options
+    })
+}
 
 
 const idAlreadyExist = (idBooking) => {
