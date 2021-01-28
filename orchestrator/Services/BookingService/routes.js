f60d8cb19047018ca8d0548a0043eccb720056c9
MaelDelaby <maeldelabouy@yahoo.fr>
2021-01-28 21:41:42 +0100

    #65 suppression des anciens fichiers

--- orchestrator/Services/BookingService/routes.js
+++ orchestrator/Services/BookingService/routes.js
@@ -1,12 +1,8 @@
 const express = require('express');
 
 const publicRouter = express.Router();
-const addBooking = require('./routes/addBooking'); 
 const bookings = require('./routes/getBookings');
-const paymentRelease = require('./routes/paymentRelease');
 
-publicRouter.use('/bookings', addBooking);
-publicRouter.use('/bookings', paymentRelease);
 publicRouter.use('/bookings', bookings);
 
 module.exports = publicRouter;
--- orchestrator/Services/TravelService/routes/getTravels.js
+++ orchestrator/Services/TravelService/routes/getTravels.js
@@ -5,39 +5,4 @@ const travelController = require('../controllers/travels'); //Fichier travels.js
 const customerController = require('../controllers/customerInfo');
 const customerApi = require('../api/customerApi');
 
-/**
- * Get all modules statics infos
- */
-travelRouter.route('/').get(async (req, res) => {
-    if (req.query.id) {
-        const customersId = customerController.parseCustomersId(req.query.id);
-        for (let i=0 ; i<customersId.length ; i++) {
-            try {
-                let customer = await customerApi.getCustomerInfo(customersId[i]);
-                if (customerController.isCustomerPMR(customer)) {
-                    req.query.pmr = true;
-                    break;
-                }
-            } catch(err) {
-                next(err);
-            }
-        }
-
-    }
-    try {
-        res.json(await travelController.getTravels(req.query));
-    } catch (err) {
-        next (err);
-    }
-});
-
-travelRouter.route('/:id').get( async (req,res) => {
-    try {
-        res.json(await travelController.getTravelById(req.params.id));
-    } catch (err) {
-        next (err);
-    }
-});
-
-
 module.exports = travelRouter;
